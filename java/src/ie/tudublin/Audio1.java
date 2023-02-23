9a2301cd0f3ed0ce4501ffdfe1c43d31bf32e4c3
C21365726 Paulina Czarnota <C21365726@mydit.ie>
2023-02-23 13:58:50 +0000

    Audio1

--- java/src/ie/tudublin/Audio1.java
+++ java/src/ie/tudublin/Audio1.java
@@ -15,6 +15,7 @@ public class Audio1 extends PApplet
 
     int mode = 0;
 
+    float[] lerpedBuffer;
     float y = 0;
     float smoothedY = 0;
     float smoothedAmplitude = 0;
@@ -35,8 +36,8 @@ public class Audio1 extends PApplet
 
     public void settings()
     {
-        size(1024, 1000, P3D);
-        //fullScreen(P3D, SPAN);
+        //size(1024, 1000, P3D);
+        fullScreen(P3D, SPAN);
     }
 
     public void setup()
@@ -45,8 +46,6 @@ public class Audio1 extends PApplet
         // Uncomment this to use the microphone
         // ai = minim.getLineIn(Minim.MONO, width, 44100, 16);
         // ab = ai.mix; 
-
-        // And comment the next two lines out
         ap = minim.loadFile("heroplanet.mp3", 1024);
         ap.play();
         ab = ap.mix;
@@ -55,8 +54,7 @@ public class Audio1 extends PApplet
         y = height / 2;
         smoothedY = y;
 
-
-        
+        lerpedBuffer = new float[width];
     }
 
     float off = 0;
@@ -73,6 +71,7 @@ public class Audio1 extends PApplet
         for(int i = 0 ; i < ab.size() ; i ++)
         {
             sum += abs(ab.get(i));
+            lerpedBuffer[i] = lerp(lerpedBuffer[i], ab.get(i), 0.05f);
         }
         average= sum / (float) ab.size();
 
@@ -89,12 +88,68 @@ public class Audio1 extends PApplet
                     //float c = map(ab.get(i), -1, 1, 0, 255);
                     float c = map(i, 0, ab.size(), 0, 255);
                     stroke(c, 255, 255);
-                    float f = ab.get(i) * halfH;
+                    float f = lerpedBuffer[i] * halfH * 4.0f;
                     line(i, halfH + f, i, halfH - f);                    
                 }
                 break;
         case 1:
-            background(0);            
+            background(0);
+            for(int i = 0 ; i < ab.size() ; i ++)
+            {
+                //float c = map(ab.get(i), -1, 1, 0, 255);
+                float c = map(i, 0, ab.size(), 0, 255);
+                stroke(c, 255, 255);
+                float f = lerpedBuffer[i] * halfH * 4.0f;
+                line(i, halfH + f, halfH - f, i);                    
+            }
+            break;
+        case 2:
+            {
+                    float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
+                    background(0, 0, 0, 10);
+                    stroke(c, 255, 255);	
+                    float radius = map(smoothedAmplitude, 0, 0.1f, 50, 300);		
+                    int points = (int)map(mouseX, 0, 255, 3, 10);
+                    int sides = points * 2;
+                    float px = cx;
+                    float py = cy - radius; 
+                    for(int i = 0 ; i <= sides ; i ++)
+                    {
+                        float r = (i % 2 == 0) ? radius : radius / 2; 
+                        // float r = radius;
+                        float theta = map(i, 0, sides, 0, TWO_PI);
+                        float x = cx + sin(theta) * r;
+                        float y = cy - cos(theta) * r;
+                        
+                        //circle(x, y, 20);
+                        line(px, py, x, y);
+                        px = x;
+                        py = y;
+                    }
+            }
+        case 3:
+            background(0);
+            strokeWeight(2);
+            noFill();
+            float r = map(smoothedAmplitude, 0, 0.5f, 100, 2000);
+            float c = map(smoothedAmplitude, 0, 0.5f, 0, 255);
+            stroke(c, 255, 255);
+            circle(cx, cy, r);
+        case 4:
+        
+            background(0);
+            strokeWeight(2);
+            for(int i = 0 ; i < ab.size() ; i +=10)
+            {
+                //float c = map(ab.get(i), -1, 1, 0, 255);
+                float cc = map(i, 0, ab.size(), 0, 255);
+                stroke(cc, 255, 255);
+                float f = lerpedBuffer[i] * halfH * 4.0f;
+                line(i, halfH + f, i, halfH - f);
+                fill(cc);
+                circle(i, halfH + f, 5);                    
+                circle(i, halfH - f, 5);                    
+            }
             break;
 
         }
@@ -108,7 +163,6 @@ public class Audio1 extends PApplet
         fill(100, 255, 255);        
         
         circle(width / 2, halfH, lerpedA * 100);
-
         circle(100, y, 50);
         y += random(-10, 10);
         smoothedY = lerp(smoothedY, y, 0.1f);        
